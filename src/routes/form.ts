6b4fa26d149ba77515b505ed451306d4d3ab6d88
Alberto <tabez@libero.it>
2022-02-06 15:02:08 +0100

    test

--- src/routes/form.ts
+++ src/routes/form.ts
@@ -8,7 +8,8 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 ];
 
 const keyring = new Keyring();
-const deadline:Number = Number(process.env["DEADLINE"]);
+console.log(process.env["DEADLINE"]);
+const deadline:Number = Number(process.env["DEADLINE"]?.toString());
 
 
 const ensureLoggedIn = (req: Request, res: Response, next: any) => {
