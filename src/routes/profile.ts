d1a784a1aab92568539e4bc3361b83d164ebe59e
Alberto <tabez@libero.it>
2022-06-14 11:23:43 +0200

    profile

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -16,19 +16,16 @@ export const profileRoutes = Router();
 profileRoutes.get("/", ensureLoggedIn, async (req, res) => {
   const id: string = (req.session as any).passport.user;
   const user = await getUser(id);
-  //if (!user) return res.render("500");
-
-  //const insertedTime = new Date(user.insertedAt).getTime() / 1000;
+  
   let userXp = user?.totalXp;
   //if (userXp == null) return res.render("500");
 
   res.render("profile", {
     username: user?.discsordUsername,
-    inOldUsers: false,
-    inKaruraSnapshot: userUtils.checkKaruraUser(user!.karuraAddress),
-    crowdcastParticipant: userUtils.checkCrowdCastPartecipant(user!.karuraCrowdLoanAddress),
-    mission1Complete: true,
-    mission2Complete: true,
-    role: 'TREK'
+    mission1Complete: user!.acalaAddress != null,
+    mission2Complete: user!.emailMission,
+    mission3Complete: user!.twitterMission,
+    inServer: user!.isInServer,
+    totalXp: userXp
   });
 });
