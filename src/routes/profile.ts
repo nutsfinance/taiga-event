b7fe4e4fbf04c20b2cb65fb174af25b3833ceeba
Alberto <tabez@libero.it>
2022-06-14 16:25:02 +0200

    profile

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -4,7 +4,6 @@ import { checkMissionTest } from "../user/usersUtils";
 const userUtils = require("../user/usersUtils");
 
 const ensureLoggedIn = (req: Request, res: Response, next: any) => {
-  next()
   if (req.isAuthenticated()) {
     next();
   } else {
@@ -18,17 +17,25 @@ profileRoutes.get("/", ensureLoggedIn, async (req, res) => {
   const id: string = (req.session as any).passport.user;
   const user = await getUser(id);
   
-  let userXp = user?.totalXp;
-  //if (userXp == null) return res.render("500");
+  let userXp = 0;
+  if (userXp == null) return res.render("500");
   let mission1Complete= checkMissionTest(user!.acalaAddress);
   let mission2Complete= checkMissionTest(user!.acalaAddress);
   let mission3Complete= checkMissionTest(user!.acalaAddress);
-  res.render("profile", {
+  let inServer = checkMissionTest(user!.acalaAddress);
+
+  if(mission1Complete)userXp=+100;
+  if(mission2Complete)userXp=+200;
+  if(mission2Complete)userXp=+200;
+  if(!mission1Complete) userXp =0;
+  if(!inServer) userXp = 0;
+
+  return res.render("profile", {
     username: user?.discsordUsername,
     mission1Complete: mission1Complete,
     mission2Complete: mission2Complete,
     mission3Complete: mission3Complete,
-    inServer: user!.isInServer,
+    inServer: inServer,
     totalXp: userXp
   });
 });
